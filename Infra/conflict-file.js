834a4318d018e97d7573d1cd740f223ed52461cb
Mat Dev <matdevtwo@gmail.com>
2022-03-16 10:23:02 -0600

    feat(app2): App2 Development
    Created app2 dir and files, introduced new function in Infra to blah blah

--- Infra/conflict-file.js
+++ Infra/conflict-file.js
@@ -1,4 +1,13 @@
-// Default File
+'use strict';
 
 // Main stay code
 console.log('Hello World!');
+
+// App2 Func
+const funcOne = () => {
+    console.log('This is funcOne');
+}
+
+module.exports = {
+    funcOne
+};
